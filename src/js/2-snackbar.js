4d00e5dfa6949500231c29f79e48df0aac1a88ac
Artek <artek.shoes@gmail.com>
2024-02-21 14:44:50 +0200

    Update 2-snackbar.js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -9,21 +9,23 @@ delayForm.addEventListener('submit', event => {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
       if (inputState === 'fulfilled') {
-        resolve(`✅ Fulfilled promise in ${inputData}ms`);
+        resolve(`✅ Fulfilled promise in ${inputData}ms          `);
       } else {
-        reject(`❌ Rejected promise in ${inputData}ms`);
+        reject(`❌ Rejected promise in ${inputData}ms          `);
       }
     }, inputData);
   });
   promise
     .then(value => {
-      iziToast.success({
+      iziToast.show({
+        color: 'green',
         message: value,
         position: 'topRight',
       });
     })
     .catch(error => {
-      iziToast.error({
+      iziToast.show({
+        color: 'red',
         message: error,
         position: 'topRight',
       });
